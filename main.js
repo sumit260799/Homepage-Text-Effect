c05cf8131ede0ce66de5400894137f05ec4b9094
sumit <sumit260799@gmail.com>
2022-04-14 19:11:55 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".typed2", {
+    strings: [
+        "Web Devoloper",
+        "Coder",
+        "Web Designer",
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+
+});
